21dda1f335727196431b48a86ad5011a20f8945e
Sergey Borovikov <serg.there@gmail.com>
2022-08-12 18:26:47 +0300

    init Auth

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,13 @@
 import { Header } from "./view/Header"
 import { Footer } from "./view/Footer"
 import { Main } from "./view/Main"
+import { Auth } from "./view/Auth"
 
 export function App() {
   return (
     <>
       <Header />
-      <Main>Too Doo</Main>
+      <Main><Auth /></Main>
       <Footer />
     </>
   )
---
+++ src/view/Auth.tsx
@@ -0,0 +1,59 @@
+import "./sass/auth.sass"
+import { useRef } from "preact/hooks"
+
+export const Auth = () => {
+
+  const emailRef = useRef<HTMLInputElement>(null)
+  const passwordRef = useRef<HTMLInputElement>(null)
+
+  const getInput = useRef(() => ([
+    emailRef.current!.value,
+    passwordRef.current!.value,
+  ])).current
+
+  const handleSignUp = async (e: MouseEvent) => {
+    e.preventDefault()
+    const [ email, password ] = getInput()
+    console.log(email, password)
+  }
+
+  const handleSignIn = async (e: MouseEvent) => {
+    e.preventDefault()
+    const [ email, password ] = getInput()
+    console.log(email, password)
+  }
+
+  return (
+    <>
+      <h1 className="auth-title">
+        Too Doo
+      </h1>
+      <form className="auth-form">
+        <input
+          className="auth-email"
+          type="email"
+          ref={emailRef}
+          placeholder="email"
+        />
+        <input
+          className="auth-password"
+          type="password"
+          ref={passwordRef}
+          placeholder="password"
+        />
+        <button
+          className="auth-signin"
+          onClick={handleSignIn}
+        >
+          sign in
+        </button>
+        <button
+          className="auth-signup"
+          onClick={handleSignUp}
+        >
+          sign up
+        </button>
+      </form>
+    </>
+  )
+}

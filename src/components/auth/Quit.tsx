b017c89a85067a75c615b72c16fa1423990bfc8e
Sergey Borovikov <serg.there@gmail.com>
2022-08-13 02:08:16 +0300

    set data to null on quit

--- src/components/auth/Quit.tsx
+++ src/components/auth/Quit.tsx
@@ -1,7 +1,14 @@
+import { setData } from "context"
 import { quit } from "service/user"
 
 export const Quit = () => {
+  
+  const handleQuit = () => {
+    quit()
+    setData(null!)
+  }
+
   return (
-    <button onClick={quit}>quit</button>
+    <button onClick={handleQuit}>quit</button>
   )
 }

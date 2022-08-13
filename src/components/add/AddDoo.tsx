a22e81d4bc3b35f319d865528acdd314b90a2ef0
Sergey Borovikov <serg.there@gmail.com>
2022-08-13 14:59:09 +0300

    pass new doo to db

--- src/components/add/AddDoo.tsx
+++ src/components/add/AddDoo.tsx
@@ -1,21 +1,26 @@
 import "./add.sass"
 import { useState } from "preact/hooks"
+import { writeToDo } from "service/data"
+import { useData } from "app/context"
 
 
 
 export const AddDoo = () => {
 
+  const data = useData()
   const [ newDoo, setNewDoo ] = useState("")
 
   const handleAddNewDoo = (e: MouseEvent) => {
     e.preventDefault()
+    const newdoo = [{ name: newDoo, done: false }]
+    const todo = newdoo.concat(data.todo)
+    writeToDo(todo)
     setNewDoo("")
   }
 
   const handleInputNewDoo = (e: Event) => {
     e.preventDefault()
     const value = (e.target as HTMLInputElement).value
-    console.log(value)
     setNewDoo(value)
   }
   

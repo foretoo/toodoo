24ad214fd270a3533281a9ec2c9f0663e4354d05
Sergey Borovikov <serg.there@gmail.com>
2022-08-13 16:14:24 +0300

    add IFilter

--- src/app/types.ts
+++ src/app/types.ts
@@ -1,4 +1,5 @@
 export type JSXChildren = JSX.Element | JSX.Element[] | string | number | null
 
+export type IFilter = "ALL" | "DONE" | "KEEN"
 export type IToDo = { name: string, done: boolean }
-export type IData = { id: string, todo: IToDo[] }
+export type IData = { id: string, filter: IFilter, todo: IToDo[] }

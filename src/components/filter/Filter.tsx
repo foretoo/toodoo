c09e20e31729b08e378c6c81f5da5bade0f6a0d7
Sergey Borovikov <serg.there@gmail.com>
2022-08-13 16:15:32 +0300

    implement db integration

--- src/components/filter/Filter.tsx
+++ src/components/filter/Filter.tsx
@@ -1,6 +1,11 @@
+import { writeFilter } from "service/data"
+import { useData } from "app/context"
+import { IFilter } from "app/types"
 import "./filter.sass"
 
 export const Filter = () => {
+
+  const { filter } = useData()
 
   const handleClick = (e: MouseEvent) => {
     e.preventDefault()
@@ -20,14 +25,19 @@ export const Filter = () => {
   }
 
   const handleFilter = (checked: boolean) => {
-    console.log(checked)
+    let newfilter: IFilter
+    if (checked)
+      newfilter = filter === "DONE" ? "ALL" : "DONE"
+    else
+      newfilter = filter === "KEEN" ? "ALL" : "KEEN"
+    writeFilter(newfilter)
   }
 
   return (
     <fieldset className="filter">
       <span>filter</span>
       <label
-        className={null ? "active" : ""}
+        className={filter === "KEEN" ? "active" : ""}
         tabIndex={0}
         onClick={handleClick}
         onKeyPress={handleKeypress}
@@ -35,13 +45,13 @@ export const Filter = () => {
         <input
           type="checkbox"
           checked={false}
-          disabled={null ? false : true}
+          disabled={filter === "KEEN" ? false : true}
           tabIndex={-1}
           onClick={(e) => e.preventDefault()}
         />
       </label>
       <label
-        className={null ? "active" : ""}
+        className={filter === "DONE" ? "active" : ""}
         tabIndex={0}
         onClick={handleClick}
         onKeyPress={handleKeypress}
@@ -49,7 +59,7 @@ export const Filter = () => {
         <input
           type="checkbox"
           checked={true}
-          disabled={null ? false : true}
+          disabled={filter === "DONE" ? false : true}
           tabIndex={-1}
           onClick={(e) => e.preventDefault()}
         />

fabb63b362b3527351a2c95f0ede0d8bb841f2ad
Sergey Borovikov <serg.there@gmail.com>
2022-08-13 22:52:59 +0300

    animate add button appearance

--- src/components/add/AddDoo.tsx
+++ src/components/add/AddDoo.tsx
@@ -1,7 +1,8 @@
 import "./add.sass"
-import { useState } from "preact/hooks"
+import { useLayoutEffect, useRef, useState } from "preact/hooks"
 import { writeToDo } from "service/data"
 import { useData } from "app/context"
+import anime from "animejs"
 
 
 
@@ -9,21 +10,49 @@ export const AddDoo = () => {
 
   const data = useData()
   const [ newDoo, setNewDoo ] = useState("")
+  const [ animateTrigger, setAnimateTrigger ] = useState(!!newDoo)
+
+
+
+  const buttonRef = useRef<HTMLButtonElement>(null!)
+  const widthRef = useRef<number>(null!)
+
+  useLayoutEffect(() => {
+    widthRef.current = buttonRef.current.getBoundingClientRect().width
+    buttonRef.current.style.marginRight = `${-widthRef.current}px`
+  }, [])
+  
+
 
   const handleAddNewDoo = (e: MouseEvent) => {
     e.preventDefault()
     const newdoo = [{ name: newDoo, done: false }]
     const todo = newdoo.concat(data.todo)
     writeToDo(todo)
+    handleAnimation(false)
     setNewDoo("")
   }
 
   const handleInputNewDoo = (e: Event) => {
     e.preventDefault()
     const value = (e.target as HTMLInputElement).value
+    handleAnimation(!!value)
     setNewDoo(value)
   }
+
+  const handleAnimation = (trigger: boolean) => {
+    if (trigger !== animateTrigger) {
+      setAnimateTrigger(trigger)
+      anime({
+        targets: buttonRef.current,
+        marginRight: `${trigger ? 0 : -widthRef.current}px`,
+        easing: "easeOutCubic"
+      })
+    }
+  }
   
+
+
   return (
     <form className="adddoo">
       <input
@@ -33,6 +62,7 @@ export const AddDoo = () => {
         placeholder="new ..."
       />
       <button
+        ref={buttonRef}
         onClick={handleAddNewDoo}
       >
         add

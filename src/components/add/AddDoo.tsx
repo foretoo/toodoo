24621a71fca4ab825add36a683d98c45593b968f
Sergey Borovikov <serg.there@gmail.com>
2022-08-13 14:56:02 +0300

    implement AddDoo

--- src/components/add/AddDoo.tsx
+++ src/components/add/AddDoo.tsx
@@ -1,5 +1,37 @@
 import "./add.sass"
+import { useState } from "preact/hooks"
+
+
 
 export const AddDoo = () => {
-  return <>Add Doo</>
+
+  const [ newDoo, setNewDoo ] = useState("")
+
+  const handleAddNewDoo = (e: MouseEvent) => {
+    e.preventDefault()
+    setNewDoo("")
+  }
+
+  const handleInputNewDoo = (e: Event) => {
+    e.preventDefault()
+    const value = (e.target as HTMLInputElement).value
+    console.log(value)
+    setNewDoo(value)
+  }
+  
+  return (
+    <form className="adddoo">
+      <input
+        type="text"
+        value={newDoo}
+        onInput={handleInputNewDoo}
+        placeholder="new ..."
+      />
+      <button
+        onClick={handleAddNewDoo}
+      >
+        add
+      </button>
+    </form>
+  )
 }

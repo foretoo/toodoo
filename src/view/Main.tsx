4c4f2b58379e578f1f7c8f98263e240ebe179712
Sergey Borovikov <serg.there@gmail.com>
2022-08-13 13:27:46 +0300

    add auth/app modes of main element

--- src/view/Main.tsx
+++ src/view/Main.tsx
@@ -1,10 +1,14 @@
 import "./sass/main.sass"
 import { JSXChildren } from "../types"
+import { useData } from "context"
 
 type MainProps = { children: JSXChildren }
 
 export const Main = (
   { children }: MainProps
 ) => {
-  return <main>{children}</main>
+
+  const data = useData()
+  
+  return <main className={data ? "app" : "auth"}>{children}</main>
 }

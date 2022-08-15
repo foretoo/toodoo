80d19c25b362b76036263356603fdaa7b329feff
Sergey Borovikov <serg.there@gmail.com>
2022-08-15 22:02:04 +0300

    add local persistence

--- src/service/init.ts
+++ src/service/init.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app"
-import { getAuth } from "firebase/auth"
+import { browserLocalPersistence, getAuth, setPersistence } from "firebase/auth"
 import { getDatabase } from "firebase/database"
 
 const firebaseConfig = {
@@ -15,5 +15,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
 export const db = getDatabase(app)
+
+setPersistence(auth, browserLocalPersistence)
 
 export const catchError = (e: unknown) => console.log(e)

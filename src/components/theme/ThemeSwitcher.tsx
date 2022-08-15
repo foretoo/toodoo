090e8b827ae6edc0043c3a9c1b7a7232ea6f1f7a
Sergey Borovikov <serg.there@gmail.com>
2022-08-15 15:24:41 +0300

    prepare controls

--- src/components/theme/ThemeSwitcher.tsx
+++ src/components/theme/ThemeSwitcher.tsx
@@ -1,5 +1,32 @@
+import { useState } from "preact/hooks"
 import "./theme-switcher.sass"
 
+
+export type Theme = "DARK" | "AUTO" | "LIGHT"
 export const ThemeSwitcher = () => {
-  return <>switcher</>
+
+  const [ theme, setTheme ] = useState<Theme>("AUTO")
+
+  return (
+    <fieldset className="theme-switcher">
+      <button
+        className={theme === "DARK" ? "active" : undefined}
+        onClick={() => setTheme("DARK")}
+        >
+          dark
+        </button>
+      <button
+        className={theme === "AUTO" ? "active" : undefined}
+        onClick={() => setTheme("AUTO")}
+        >
+          auto
+        </button>
+      <button
+        className={theme === "LIGHT" ? "active" : undefined}
+        onClick={() => setTheme("LIGHT")}
+        >
+          light
+        </button>
+    </fieldset>
+  )
 }

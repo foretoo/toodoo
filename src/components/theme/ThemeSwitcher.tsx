7aa1bde2d3ab8744580efa3f3c3c009c01772d42
Sergey Borovikov <serg.there@gmail.com>
2022-08-15 18:22:55 +0300

    ThemeSwitcher ready

--- src/components/theme/ThemeSwitcher.tsx
+++ src/components/theme/ThemeSwitcher.tsx
@@ -1,12 +1,68 @@
-import { useState } from "preact/hooks"
-import { SwitchButton } from "./SwitchButton"
 import "./theme-switcher.sass"
+import { useEffect, useState } from "preact/hooks"
+import { SwitchButton } from "./SwitchButton"
 
 
-export type ITheme = "DARK" | "AUTO" | "LIGHT"
+
+const rule = {
+  dark:
+    "background-color: black; "  +
+    "--bg: var(--light-1); "     +
+    "--fill: #222; "             +
+    "--key: #444; "              +
+    "--text: var(--light-5); "   +
+    "--text-m: var(--light-3); " +
+    "--text-l: var(--light-2)",
+  light:
+    "background-color: #eee; "    +
+      "--bg: var(--dark-1); "     +
+      "--fill: white; "           +
+      "--key: white; "            +
+      "--text: var(--dark-5); "   +
+      "--text-m: var(--dark-3); " +
+      "--text-l: var(--dark-2)",
+}
+
+const localTheme: ILocalTheme = localStorage.getItem("theme") as ILocalTheme | undefined || "AUTO"
+
+export type ILocalTheme = "DARK" | "AUTO" | "LIGHT"
+export type IAppTheme = "DARK" | "LIGHT"
 export const ThemeSwitcher = () => {
 
-  const [ theme, setTheme ] = useState<ITheme>("AUTO")
+  const [ trigger, forceUpdate ] = useState(false)
+  const [ theme, setTheme ] = useState<ILocalTheme>(localTheme)
+
+  let appTheme: IAppTheme
+
+  if (theme === "AUTO")
+    appTheme =
+      window.matchMedia
+      ? window.matchMedia("(prefers-color-scheme: dark)").matches
+        ? "DARK"
+        : "LIGHT"
+      : "LIGHT"
+  else appTheme = theme
+
+
+
+  useEffect(() => {
+    if (!window.matchMedia) return
+    const themeHandler = () => theme === "AUTO" && forceUpdate(!trigger)
+    const darkSchemeQuery = window.matchMedia("(prefers-color-scheme: dark)")
+    darkSchemeQuery.addEventListener("change", themeHandler)
+    return () => darkSchemeQuery.removeEventListener("change", themeHandler)
+  }, [])
+
+
+
+  useEffect(() => {
+    const body = document.body as HTMLBodyElement
+    body.setAttribute("style", appTheme === "DARK" ? rule.dark : rule.light)
+    if (theme !== "AUTO") localStorage.setItem("theme", theme)
+    else localStorage.removeItem("theme")
+  })
+
+
 
   return (
     <fieldset className="theme-switcher">

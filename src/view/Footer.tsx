5d6ebb6f3e467cb9ed99c8aa4419ff2ca20550ca
Sergey Borovikov <serg.there@gmail.com>
2022-08-15 15:24:06 +0300

    compose ThemeSwitcher

--- src/view/Footer.tsx
+++ src/view/Footer.tsx
@@ -1,3 +1,10 @@
+import "./sass/footer.sass"
+import { ThemeSwitcher } from "components/theme"
+
 export const Footer = () => {
-  return <footer></footer>
+  return (
+    <footer>
+      <ThemeSwitcher />
+    </footer>
+  )
 }

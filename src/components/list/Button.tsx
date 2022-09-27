1ac32ff06de26458177219c64098ff049d004563
Sergey Borovikov <serg.there@gmail.com>
2022-09-28 01:43:54 +0300

    provide ref type

--- src/components/list/Button.tsx
+++ src/components/list/Button.tsx
@@ -4,7 +4,7 @@ export const Button = (
   { id, over, deleteDoo }: { id: number, over: boolean, deleteDoo: (id: number) => void }
 ) => {
 
-  const buttonRef = useHoverAnimation(over)
+  const buttonRef = useHoverAnimation<HTMLButtonElement>(over)
 
   return (
     <button
